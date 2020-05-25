85c1cda79c4e7368acec3fe66f53da5605b12543
shakti chauhan <shakti@shaktis-iMac-Pro.local>
2020-05-26 00:31:40 +0400

    updating src

--- react-redux/marioplan/src/App.js
+++ react-redux/marioplan/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 class App extends Component {
   render() {
@@ -12,7 +15,10 @@ class App extends Component {
 
           {/* //ensures only one is loaded up */}
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/Sigin" component={SignIn} />
+            <Route path="/SignUp" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
